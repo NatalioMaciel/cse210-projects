1200ac6990b5dcd3df6a449774525325f309ac33
NatalioMaciel <mac24026@byui.edu>
2025-05-14 15:30:25 -0300

    Exercise 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,30 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numberList = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter a number: ");
+        int numbers = int.Parse(Console.ReadLine());
+
+        while (numbers != 0)
+        {
+            numberList.Add(numbers);
+            Console.Write("Enter a number: ");
+            numbers = int.Parse(Console.ReadLine());
+
+        }
+
+        int sumNumbers = numberList.Sum();
+        double average = Queryable.Average(numberList.AsQueryable());
+        int largest = numberList.Max();
+
+
+        Console.WriteLine($"Te sum is: {sumNumbers}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
