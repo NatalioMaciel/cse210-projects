9a95018f5013b922e4411cad8e46af8eb4489044
NatalioMaciel <mac24026@byui.edu>
2025-05-14 13:48:49 -0300

    Exercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage ? ");
+        string userGrade = Console.ReadLine();
+
+        int grade = int.Parse(userGrade);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations on passing the course!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Good luck for the next block. You can do it!");
+        }
     }
 }
