df631ad8bae1658c1730646732dcef0ae4f837e6
NatalioMaciel <mac24026@byui.edu>
2025-05-15 21:52:59 -0300

    Resumes

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+using System.IO.Compression;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
