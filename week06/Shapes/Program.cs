3c4e35e7db5c889bea1378898776c774932811ee
NatalioMaciel <mac24026@byui.edu>
2025-06-17 22:47:03 -0300

    Shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Dynamic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Red", 10);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 7, 2);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 55);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
+
+
 }
